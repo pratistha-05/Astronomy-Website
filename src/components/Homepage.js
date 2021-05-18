b735990da494711622a750c4238fd3f669e775fe
Pratistha <rinsin2002@gmail.com>
2021-05-18 17:53:14 +0530

    homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,30 +8,50 @@ import history from '../history';
 
 
 const useStyles = makeStyles((theme) => ({
+   
    title: {
       flexGrow: 1,
       color: "#ffffff"
     },
+    root:
+    {
+      backgroundColor:'red'
+    },
+    text:
+    {
+      color:'white',
+      fontStyle: 'arial',   
+      fontSize:100,
+      letterSpacing:0.7,
+    },
+   
   }));
   
   export default function Homepage() {
     const classes = useStyles();
   
     return (
-      <div className={classes.root}>
+      <div style={{backgroundImage:"url("+"https://images.wallpaperscraft.com/image/space_planet_astronomy_133670_3840x2160.jpg"+")",
+    backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat',height:'100vh',objectFit:'cover',width:'auto'
+      }}>
         <AppBar style={{backgroundColor: '#BB8FCE'}}position="static">
           <Toolbar>
           
-            <Typography variant="h6" className={classes.title} onClick={() => history.push('/date_and_timings')}>
+            <Button variant="h6" //className={classes.title}
+             onClick={() => history.push('/date_and_timings')}>
               Todays Timings
-            </Typography>
-            <Typography variant="h6" className={classes.title}  onClick={() => history.push('/pictures')}>
+            </Button>
+            <Button variant="h6" //className={classes.title}  
+            onClick={() => history.push('/pictures')}>
               Pic Of The Day
-            </Typography>
+            </Button>
             
-            <Button variant="outlined">Rate</Button>
+            <Button >Rate</Button>
           </Toolbar>
         </AppBar>
+        <div style={{marginTop:'200px',marginleft:'100px'}}>
+    <Typography className={classes.text}><span style={{ fontSize:30,}}>WHO ARE </span>ARE WE?</Typography>
+    </div>
       </div>
     );
   }
