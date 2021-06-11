6740823ccf0de1edd901bbb93b4742d6ddc31c9a
Pratistha <rinsin2002@gmail.com>
2021-06-11 13:05:48 +0530

    Carousel

--- src/components/CarouselSlides.js
+++ src/components/CarouselSlides.js
@@ -3,43 +3,80 @@ import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { Typography } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {makeStyles,createStyles,withStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    carousel1:{
-        position: 'relattive',
-    },
+   
     text:
     {
         position: 'absolute',
-        right: '50%',
-        left: '50%',
+        right: '10%',
+        left: '60%',
         bottom: '15%',
+        color:'white'
     },
+    text1:{
+        fontSize:80,
+        marginBottom:-8
+    },
+    heading:{
+        position: 'absolute',
+        right: '65%',
+        left: '0%',
+        bottom: '80%',
+        color:'white'
+
+    }
    
 }));
 
-
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Ranchers'
+      ].join(','),
+    },
+   
+    });
 
 export default function CarouselSlides(){
     const classes = useStyles();
 
     return(
 
-        <Carousel showThumbs={false} showArrows={false} autoPlay >
+        <Carousel showThumbs={false} autoPlay infiniteLoop interval="5000">
         <div >
-           <div className={classes.carousel1}>
+           <div>
             <img src="https://coverfiles.alphacoders.com/494/49416.jpg"/>
             </div>
             <div className={classes.text}>
-            <TextField id="standard-basic" label="Standard" />
-            <p>hooooollllllllllaaaaaa</p>
+           <h3 className={classes.text1}><span style={{ fontSize:30}}>WHO ARE</span> WE? </h3>
+               <p>
+               <b><i>THE HUMAN RACE IS JUST A CHEMICAL SCUM ON A MODERATE-SIZED PLANET, ORBITING AROUND A VERY AVERAGE STAR IN THE OUTER SUBURB OF ONE AMONG A HUNDRED BILLION GALAXIES.</i></b>
+               </p>
+            </div>
+            <div className={classes.heading}>
+                <ThemeProvider theme={theme}>
+            <Typography variant="h4">Somewhere, Something incredible is waiting to be known</Typography>
+            </ThemeProvider>
             </div>
         </div>
         <div>
+            <div>
             <img src="https://png.pngtree.com/thumb_back/fw800/back_our/20190622/ourmid/pngtree-purple-gradient-flat-space-planet-banner-background-image_229576.jpg" />
-            <p className="legend">Legend 2</p>
+            </div>
+            <div className={classes.text}>
+            <h3 className={classes.text1}><span style={{ fontSize:30}}>WHERE ARE</span> WE?</h3>
+               <p>
+               <b><i>WE FIND THAT WE LIVE IN AN INSIGNIFICANT PLANET OF A HUMDRUM STAR LOST IN A GALAXY TUCKED AWAY IN A CORNOR OF A UNIVERSE IN WHICH THERE ARE FAR MORE GALAXIES THAN PEOPLE</i></b>
+            </p>
+            </div>
+            <div className={classes.heading}>
+                <ThemeProvider theme={theme}>
+            <Typography variant="h4">Somewhere, Something incredible is waiting to be known</Typography>
+            </ThemeProvider>
+            </div>
         </div>
         
     </Carousel>
